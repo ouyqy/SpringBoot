e4d01b800a4585b0dd226c22ea0a9fc4fe4fa9e2
oyqy <ouyqy@physicalpoint.com>
2017-09-14 13:50:44 +0800

    junit test

---
+++ SpringBootTest/src/main/java/com/avalon/bean/TestBean.java
@@ -0,0 +1,20 @@
+package com.avalon.bean;
+
+/**
+ * Created by ouyqy on 2017/9/14.
+ */
+public class TestBean {
+    private String content;
+
+    public TestBean(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
--- SpringBootTest/src/test/java/com/avalon/SpringBootTestApplicationTests.java
+++ SpringBootTest/src/test/java/com/avalon/SpringBootTestApplicationTests.java
@@ -1,16 +1,27 @@
 package com.avalon;
 
+import com.avalon.bean.TestBean;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("dev")
 public class SpringBootTestApplicationTests {
+
+	@Autowired
+	private TestBean testBean;
 
 	@Test
 	public void contextLoads() {
+		String expected = "from development profile";
+		String actual = testBean.getContent();
+		Assert.assertEquals(expected,actual);
 	}
 
 }
