5e2f511332f082877b04b55a2e75f6dfce782525
oyqy <ouyqy@physicalpoint.com>
2017-09-18 16:10:59 +0800

    setUseSuffixPatternMatch(false)

--- SpringBootCommonAnnotaion/src/main/java/com/avalon/config/DemoInterceptorBean.java
+++ SpringBootCommonAnnotaion/src/main/java/com/avalon/config/DemoInterceptorBean.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
@@ -17,6 +18,12 @@ public class DemoInterceptorBean extends WebMvcConfigurerAdapter{
     @Bean //配置拦截器的bean
     public DemoInterceptor demoInterceptor(){
         return new DemoInterceptor();
+    }
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        //获取路径后缀pathVar/xxx.xx ; default 获取为xxx,加上setUseSuffixPatternMatch(false) 获取为xxx.xx
+        configurer.setUseSuffixPatternMatch(false);
     }
 
     //重写addInterceptor方法,注册拦截器
