5867046e4065dbcfcf55ef7640e62155e8f97e95
oyqy <ouyqy@physicalpoint.com>
2017-12-08 17:02:51 +0800

    bootstrap

---
+++ boostrapangular/src/main/java/com/avalon/ouyqy/boostrapangular/BoostrapangularApplication.java
@@ -0,0 +1,12 @@
+package com.avalon.ouyqy.boostrapangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoostrapangularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoostrapangularApplication.class, args);
+	}
+}
